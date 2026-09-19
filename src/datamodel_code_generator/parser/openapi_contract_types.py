4ea1c96253658f7c81deced6baa6d5e4127d651f
Koudai Aono <koxudaxi@gmail.com>
2026-09-19 17:25:33 +0900

    Make binding type narrowing explicit

--- src/datamodel_code_generator/parser/openapi_contract_types.py
+++ src/datamodel_code_generator/parser/openapi_contract_types.py
@@ -78,8 +78,8 @@ def _builtin_type(name: str) -> BuiltinType:
             | "frozenset"
             | "dict"
             | "tuple"
-        ):
-            return BuiltinType(name)
+        ) as builtin:
+            return BuiltinType(builtin)
         case _:
             raise _UnsupportedTypeError(_TYPE_UNSUPPORTED)
 
